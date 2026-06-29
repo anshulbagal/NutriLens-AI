da70bdd18fb608aa3c0300ea5b5a956a1b6b2d32
anshulbagal <anshulbagal5@gmail.com>
2026-06-29 20:19:36 +0530

    Fix LangChain text splitter import

--- backend/app/rag/retriever.py
+++ backend/app/rag/retriever.py
@@ -6,7 +6,7 @@ import os
 import glob
 
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
